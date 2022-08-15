eddeaacf041296fe3a6458eaf31c96e73f2573f6
Stefanie Voelker <karriere@stefanie-voelker.de>
2022-08-15 18:08:05 +0200

    implemented day02 (first task)

--- Common/Program.cs
+++ Common/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AoCDay01;
+using AoCDay02;
 
 namespace Common
 {
@@ -11,6 +12,10 @@ namespace Common
             Day01 day01 = new Day01();
             Console.WriteLine("Result Task 1: "+ day01.ExecuteFirstTask());
             Console.WriteLine("Result Task 2: "+ day01.ExecuteSecondTask());
+
+            Console.WriteLine("=== Day 02 ===");
+            Day02 day02 = new Day02();
+            Console.WriteLine("Result Task 1: "+ day02.ExecuteFirstTask());
         }
     }
 }
---
+++ Day02/Day02.cs
@@ -0,0 +1,46 @@
+﻿namespace AoCDay02;
+public class Day02
+{
+    public int ExecuteFirstTask()
+    {
+        var data = ReadInput();
+        (int horizontalPosition, int depth) = PerformBasicMovement(data);
+        return horizontalPosition * depth;
+    }
+
+    public int ExecuteSecondTask()
+    {
+        return 0;
+    }
+
+    public List<string> ReadInput()
+    {
+        var input = File.ReadAllLines(Path.Combine("..", "Day02", "Data","input.txt")).ToList();
+        return input;
+    }
+
+    private (int, int) PerformBasicMovement(List<string> data)
+    {
+        int horizontalPosition = 0;
+        int depth = 0;
+        foreach(var item in data)
+        {
+            var direction = item.Split(' ')[0];
+            var distance = Convert.ToInt32( item.Split(' ')[1]);
+            switch(direction)
+            {
+                case "forward":
+                    horizontalPosition += distance;
+                    break;
+                case "up":
+                    depth -= distance;
+                    break;
+                case "down":
+                    depth += distance;
+                    break;
+            }
+        }
+
+        return (horizontalPosition, depth);
+    }
+}
